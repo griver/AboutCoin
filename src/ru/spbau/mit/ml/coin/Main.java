bab686ee8a611da689b1b2f0799063f378fff400
griver <griver29@gmail.com>
2013-01-03 19:47:03 +0400

    hello commit

---
+++ src/ru/spbau/mit/ml/coin/Main.java
@@ -0,0 +1,14 @@
+package ru.spbau.mit.ml.coin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: griver
+ * Date: 03.01.13
+ * Time: 19:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    static public void main(String[] argv) {
+        System.out.print("hello");
+    }
+}
